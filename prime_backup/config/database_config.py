53664e2414511a3656bea8f5c2c07feb5dc55586
Fallen_Breath <yqx791125833@gmail.com>
2023-12-06 20:31:53 +0800

    enable db compact jon by default

--- prime_backup/config/database_config.py
+++ prime_backup/config/database_config.py
@@ -4,9 +4,9 @@ from prime_backup.types.units import Duration
 
 
 class CompactDatabaseConfig(Serializable):
-	enabled: bool = False
-	interval: Duration = Duration('12h')
-	jitter: Duration = Duration('1m')
+	enabled: bool = True
+	interval: Duration = Duration('1d')
+	jitter: Duration = Duration('5m')
 
 
 class DatabaseConfig(Serializable):

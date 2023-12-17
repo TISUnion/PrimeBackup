95c6e14d4e687787d3fb4a2b336f96f71f8643ad
Fallen_Breath <yqx791125833@gmail.com>
2023-12-18 00:35:19 +0800

    properly handle cross-device file copy in linux
    resolved #6

--- prime_backup/utils/file_utils.py
+++ prime_backup/utils/file_utils.py
@@ -1,3 +1,4 @@
+import errno
 import os
 import shutil
 import stat
@@ -13,11 +14,19 @@ HAS_COPY_FILE_RANGE = callable(getattr(os, 'copy_file_range', None))
 
 def copy_file_fast(src_path: Path, dst_path: Path):
 	if HAS_COPY_FILE_RANGE:
-		with open(src_path, 'rb') as f_src, open(dst_path, 'wb+') as f_dst:
-			while os.copy_file_range(f_src.fileno(), f_dst.fileno(), 2 ** 30):
-				pass
-	else:
-		shutil.copyfile(src_path, dst_path, follow_symlinks=False)
+		try:
+			with open(src_path, 'rb') as f_src, open(dst_path, 'wb+') as f_dst:
+				while os.copy_file_range(f_src.fileno(), f_dst.fileno(), 2 ** 30):
+					pass
+			return
+		except OSError as e:
+			# https://man7.org/linux/man-pages/man2/copy_file_range.2.html
+			if e.errno in [errno.EXDEV, errno.EOPNOTSUPP]:
+				pass  # retry with shutil.copyfile
+			else:
+				raise
+
+	shutil.copyfile(src_path, dst_path, follow_symlinks=False)
 
 
 def rm_rf(path: Path, *, missing_ok: bool = False):

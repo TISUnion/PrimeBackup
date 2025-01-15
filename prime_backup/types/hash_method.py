b7b41541a1d161ccda1f5db7092c5f5846e8fa8d
Fallen_Breath <nellaforax@outlook.com>
2025-01-15 11:25:20 +0800

    HashMethod add md5
    for future benchmark purpose

--- prime_backup/types/hash_method.py
+++ prime_backup/types/hash_method.py
@@ -26,5 +26,22 @@ class _HashMethodItem:
 
 class HashMethod(enum.Enum):
 	xxh128 = _HashMethodItem('xxhash.xxh128', 32)
+	md5 = _HashMethodItem('hashlib.md5', 32)
 	sha256 = _HashMethodItem('hashlib.sha256', 64)
 	blake3 = _HashMethodItem('blake3.blake3', 64)
+
+
+def __verify_hex_length():
+	for hash_method in HashMethod:
+		try:
+			hasher = hash_method.value.create_hasher()
+		except ImportError:
+			continue
+		else:
+			hasher.update(b'foo')
+			s = hasher.hexdigest()
+			if len(s) != hash_method.value.hex_length:
+				raise AssertionError('{} declares hex_length={}, but the actual hex length is {}'.format(hash_method.value, hash_method.value.hex_length, len(s)))
+
+
+__verify_hex_length()

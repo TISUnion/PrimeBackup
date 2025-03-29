0194a6ede0e75d6884e63360bc35cfff9a10891f
Fallen_Breath <nellaforax@outlook.com>
2025-03-30 02:35:50 +0800

    The #64 fixer

--- tools/README.md
+++ tools/README.md
@@ -31,3 +31,165 @@ optional arguments:
   -s SLOT, --slot SLOT  Specified the slot number to import. If not provided, import all slots
                         (default: None)
 ```
+
+## [issue_64_fixer.py](issue_64_fixer.py)
+
+A data-recover helper tool for [Issue #64](https://github.com/TISUnion/PrimeBackup/issues/64)
+
+> [!NOTE]  
+> You need to have external backups of the database file (created by PB v1.9+) and optional some blob store (`pb_files/blobs`) backups to perform the fix
+> If no backup, then no rescue :(
+
+```bash
+$ python3 ./issue_64_fixer.py --help
+usage: issue_64_fixer.py [-h] --pb-plugin PB_PLUGIN --pb-dir PB_DIR
+                         [--db-backup-dir DB_BACKUP_DIR] [--blobs-backup-dir BLOBS_BACKUP_DIR]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --pb-plugin PB_PLUGIN
+                        Path to the Prime Backup plugin file (default: None)
+  --pb-dir PB_DIR       Path to the pb_files directory (default: None)
+  --db-backup-dir DB_BACKUP_DIR
+                        Path to the directory that contains database backup files. All .db files    
+                        inside will be used. You can ignore this argument if you dont need to       
+                        recover database file objects (default: None)
+  --blobs-backup-dir BLOBS_BACKUP_DIR
+                        Path to the directory that contains blobs directory backups. Directories    
+                        inside should be something like blobs1, blobs2, blobs3, where each
+                        directory is a valid blobs directory from pb_files/blobs. You can ignore    
+                        this argument if you dont need to recover blobs (default: None)
+```
+
+### Prepare
+
+> [!IMPORTANT]
+> This tool assert that no hash method / compress method migration was done after those external data backups are made
+
+First, confirm the time you upgraded your Prime Backup plugin to affected version (v1.9.0 ~ v1.9.2). Let's say you upgraded PrimeBackup at time T0
+
+Second, you need to collect external backups of these 2 things since T0:
+
+1. Prime Backup database file, i.e. `prime_backup.db`. By default, Prime Backup enables a scheduled database backup feature, 
+   and creates a few database backups inside `/path/to/pb_files/db_backup`. You can try using these database backup files
+2. (Optional, you can first try without) Prime Backup blob store backups. i.e. backups of `pb_files/blobs`.
+   External backups / VM backups / Disk backup, try your best to collect them 
+
+Then categorize them into folders for storage, such as:
+
+```
+db_backups/
++-- prime_backup_20250317.db
++-- prime_backup_20250324.db
++-- ...
+
+blobs_backups/
++-- blobs_20250317
+    +-- 00
+        +-- 000a9e1e9110bbbabe023114f4eb1c59029aa255682b5f1100fd454a5ace95c5
+    +-- 01
+    +-- ...
++-- blobs_20250324
++-- ...
+```
+
+### Run it
+
+> [!TIP]
+> Before run anything, back everything up (the `pb_files` directory), in case of something bad happens
+
+First, you need to locate the Prime Backup instance you want to fix (e.g. path to the `pb_files` directory), and prepare a valid Prime Backup plugin file
+
+> [!NOTE]  
+> The tool is tested with Prime Backup v1.9.4. Newer versions might still work
+
+Then, enter a python environment with all Prime Backup's required python packages installed.
+Now you can run the fix tool by:
+
+```bash
+python3 ./issue_64_fixer.py --pb-plugin /path/to/PrimeBackup-v1.9.4.pyz --pb-dir /path/to/pb_files --db-backup-dir ./db_backups --blobs-backup-dir ./blobs_backups
+# or multiline
+python3 ./issue_64_fixer.py \
+  --pb-plugin /path/to/PrimeBackup-v1.9.4.pyz \
+  --pb-dir /path/to/pb_files \
+  --db-backup-dir ./db_backups \
+  --blobs-backup-dir ./blobs_backups
+```
+
+You can omit `--blobs-backup-dir ./blobs_backups` if you don't have any blob store backups yet.
+In some lucky cases, there might be 0 affected blob and no blob restore is required
+
+The script is re-enter-able. Feel free to re-run it if you collect more external backups
+
+Example script output with comments:
+
+```bash
+# This '/path/to/pb_files' should be the Prime Backup instance you want to fix, value from --pb-dir
+[2025-03-30 01:49:05,402 INFO] (__init_pb_environment) Storage root set to '/path/to/pb_files'
+
+# First, the script will perform a fileset validation to figure out those corrupt filesets
+[2025-03-30 01:49:05,423 INFO] (__get_bad_base_fileset_ids) Locating bad fileset ids
+[2025-03-30 01:49:05,423 INFO] (run) Fileset validation start
+[2025-03-30 01:49:05,424 INFO] (run) Validating 492 fileset objects
+[2025-03-30 01:49:05,426 INFO] (run) Validating 200 / 492 fileset objects
+[2025-03-30 01:49:06,747 INFO] (run) Validating 400 / 492 fileset objects
+[2025-03-30 01:49:07,953 INFO] (run) Validating 492 / 492 fileset objects
+[2025-03-30 01:49:08,228 INFO] (run) Fileset validation done: total 492, validated 492, ok 434, bad 58
+
+# Here's the file validation result
+# We only need to fix the base filesets
+[2025-03-30 01:49:08,228 INFO] (__get_bad_base_fileset_ids) Found 58 bad filesets
+[2025-03-30 01:49:08,228 INFO] (__get_bad_base_fileset_ids) Found 2 bad base filesets
+[2025-03-30 01:49:08,228 INFO] (__get_bad_base_fileset_ids) 399, 451
+
+# Then, the script will try its best to recover file objects for these bad filesets, from all database files inside --db-backup-dir
+[2025-03-30 01:49:08,279 INFO] (__read_backup_db) Read 11075 file rows for fileset 399 from database '/path/to/db_backups/prime_backup_20250317.db'
+[2025-03-30 01:49:08,320 INFO] (__read_backup_db) Read 11217 file rows for fileset 451 from database '/path/to/db_backups/prime_backup_20250317.db'
+[2025-03-30 01:49:08,324 INFO] (__read_backup_db) Read 265 file rows for fileset 399 from database '/path/to/db_backups/prime_backup_20250324.db'
+[2025-03-30 01:49:08,364 INFO] (__read_backup_db) Read 11217 file rows for fileset 451 from database '/path/to/db_backups/prime_backup_20250324.db'
+[2025-03-30 01:49:08,367 INFO] (run) Recovering files for base fileset 399
+[2025-03-30 01:49:08,583 INFO] (run) Recovering 10810 files for fileset 399
+[2025-03-30 01:49:08,584 INFO] (run) Recovering files for base fileset 451
+[2025-03-30 01:49:09,510 INFO] (run) Recovering 11154 files for fileset 451
+[2025-03-30 01:49:09,861 INFO] (run) Recovered 21964 files in total
+
+# File object recover done. Perform another validation to double check the state
+[2025-03-30 01:49:09,861 INFO] (run) Perform another filesets validation since new files were added
+[2025-03-30 01:49:09,861 INFO] (run) Fileset validation start
+[2025-03-30 01:49:09,862 INFO] (run) Validating 492 fileset objects
+[2025-03-30 01:49:09,862 INFO] (run) Validating 200 / 492 fileset objects
+[2025-03-30 01:49:11,215 INFO] (run) Validating 400 / 492 fileset objects
+[2025-03-30 01:49:12,408 INFO] (run) Validating 492 / 492 fileset objects
+[2025-03-30 01:49:13,079 INFO] (run) Fileset validation done: total 492, validated 492, ok 492, bad 0
+[2025-03-30 01:49:13,080 INFO] (run) NICE, fileset fix done
+
+# Rebuild those missing blob object in the database
+[2025-03-30 01:49:13,269 INFO] (run) Recovered 9 database blob for fileset 399
+[2025-03-30 01:49:13,437 INFO] (run) Recovered 0 database blob for fileset 451
+[2025-03-30 01:49:13,449 INFO] (run) Recovered 9 database blob in total
+
+# Now it's the time to recover those deleted blob files in the blob store
+# if --blobs-backup-dir is not provided, this step will be skipped
+[2025-03-30 01:49:14,125 INFO] (__find_blob_store_roots) Searching at 'blobs_backups' for blob storage roots, for hash_method blake3 with hex length 64
+[2025-03-30 01:49:14,125 INFO] (__find_blob_store_roots) Found blob store root at '/path/to/blobs_backups/blobs_20250324'
+[2025-03-30 01:49:14,125 INFO] (__find_blob_store_roots) Found 1 blob store roots in total
+[2025-03-30 01:49:14,126 INFO] (run) Iterating all blobs in database to check if there is any missing blobs
+[2025-03-30 01:49:14,644 INFO] (run) Found 9 blobs with their file missing, and recovered 9 blob files in total
+
+# Blob file recover done, perform a blob validation as a double check
+[2025-03-30 01:49:14,653 INFO] (run) Perform a blob validation since new files were added
+[2025-03-30 01:49:14,653 INFO] (run) Blob validation start
+[2025-03-30 01:49:14,678 INFO] (run) Validating 3000 / 31765 blobs
+[2025-03-30 01:49:15,643 INFO] (run) Validating 6000 / 31765 blobs
+[2025-03-30 01:49:17,689 INFO] (run) Validating 9000 / 31765 blobs
+[2025-03-30 01:49:18,995 INFO] (run) Validating 12000 / 31765 blobs
+[2025-03-30 01:49:30,261 INFO] (run) Validating 15000 / 31765 blobs
+[2025-03-30 01:49:41,155 INFO] (run) Validating 18000 / 31765 blobs
+[2025-03-30 01:49:52,696 INFO] (run) Validating 21000 / 31765 blobs
+[2025-03-30 01:50:03,684 INFO] (run) Validating 24000 / 31765 blobs
+[2025-03-30 01:50:13,825 INFO] (run) Validating 27000 / 31765 blobs
+[2025-03-30 01:50:24,497 INFO] (run) Validating 30000 / 31765 blobs
+[2025-03-30 01:50:35,301 INFO] (run) Validating 31765 / 31765 blobs
+[2025-03-30 01:50:41,262 INFO] (run) Blob validation done: total 31765, validated 31765, ok 31765, bad 0
+[2025-03-30 01:50:41,263 INFO] (run) NICE, blob file fix done
+```
---
+++ tools/issue_64_fixer.py
@@ -0,0 +1,296 @@
+#!/usr/bin/env python3
+"""
+https://github.com/TISUnion/PrimeBackup/issues/64
+Tested and works with Prime Backup v1.9.4
+"""
+import argparse
+import collections
+import logging
+import os
+import re
+import shutil
+import sys
+from pathlib import Path
+from typing import Dict, List, Optional, Set
+
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
+
+FilesetFileBackups = Dict[int, Dict[str, dict]]  # fileset_id -> (path -> row dict)
+
+
+class DatabaseFilesetFileFixer:
+	def __init__(self, bad_base_fileset_ids: List[int], db_backup_dir: Path):
+		self.bad_base_fileset_ids = bad_base_fileset_ids
+		self.db_backup_dir = db_backup_dir
+
+		from prime_backup import logger
+		self.logger: logging.Logger = logger.get()
+
+	def __read_backup_db(self, db_path: Path) -> FilesetFileBackups:
+		try:
+			engine = create_engine('sqlite:///' + str(db_path))
+			with Session(engine) as session:
+				db_fileset_files: FilesetFileBackups = collections.defaultdict(dict)
+				for fileset_id in self.bad_base_fileset_ids:
+					file_rows = list(session.execute(text(f'SELECT * FROM file WHERE fileset_id = {fileset_id}')))
+					for file_row in file_rows:
+						# noinspection PyProtectedMember
+						row = file_row._mapping
+						db_fileset_files[fileset_id][row['path']] = dict(row)
+					self.logger.info('Read {} file rows for fileset {} from database {!r}'.format(len(file_rows), fileset_id, str(db_path)))
+				return db_fileset_files
+		except Exception as e:
+			self.logger.exception('Failed to read from db database {!r}: {}'.format(repr(str(db_path)), e))
+			sys.exit(1)
+
+	def __read_files_in_backup_dbs(self) -> FilesetFileBackups:
+		if not self.db_backup_dir.is_dir():
+			print('Invalid db backup directory: {}'.format(self.db_backup_dir))
+			sys.exit(1)
+
+		merged_file_backups: FilesetFileBackups = collections.defaultdict(dict)
+		for file_name in os.listdir(self.db_backup_dir):
+			file_path = self.db_backup_dir / file_name
+			if file_path.is_file() and file_path.suffix == '.db':
+				single_file_backups = self.__read_backup_db(file_path)
+				for fileset_id, files in single_file_backups.items():
+					merged_file_backups[fileset_id].update(files)
+
+		return merged_file_backups
+
+	def run(self):
+		good_fileset_files = self.__read_files_in_backup_dbs()
+
+		new_file_count = 0
+
+		from prime_backup.db.access import DbAccess
+		with DbAccess.open_session() as session:
+			for fileset_id in self.bad_base_fileset_ids:
+				self.logger.info('Recovering files for base fileset {}'.format(fileset_id))
+				existing_file_paths = {file.path for file in session.get_fileset_files(fileset_id)}
+				good_file_for_this_fileset = good_fileset_files.get(fileset_id, {})
+				new_file_this_fileset_count = 0
+				for path, file_row in good_file_for_this_fileset.items():
+					if path not in existing_file_paths:
+						self.logger.debug('Recovering file at {!r} for fileset {}'.format(path, fileset_id))
+						session.add(session.create_file(**file_row))
+						new_file_count += 1
+						new_file_this_fileset_count += 1
+				self.logger.info('Recovering {} files for fileset {}'.format(new_file_this_fileset_count, fileset_id))
+
+		self.logger.info('Recovered {} files in total'.format(new_file_count))
+		if new_file_count > 0:
+			from prime_backup.action.validate_filesets_action import ValidateFilesetsAction
+			self.logger.info('Perform another filesets validation since new files were added')
+			result = ValidateFilesetsAction().run()
+			if len(result.bad_filesets) == 0:
+				self.logger.info('NICE, fileset fix done')
+			else:
+				self.logger.info('There are still some bad filesets')
+				with DbAccess.open_session() as session:
+					fileset_ids_by_kind: Dict[bool, List[int]] = collections.defaultdict(list)  # is_base -> fileset_ids
+					filesets = session.get_filesets(list(result.bad_filesets.keys()))
+					for fileset in filesets.values():
+						fileset_ids_by_kind[fileset.base_id == 0].append(fileset.id)
+				self.logger.info('Bad base filesets: {}'.format(', '.join(map(str, fileset_ids_by_kind[True]))))
+				self.logger.info('Bad delta filesets: {}'.format(', '.join(map(str, fileset_ids_by_kind[False]))))
+
+
+class DatabaseBlobFixer:
+	def __init__(self, bad_base_fileset_ids: List[int]):
+		self.bad_base_fileset_ids = bad_base_fileset_ids
+
+		from prime_backup import logger
+		self.logger: logging.Logger = logger.get()
+
+	def run(self):
+		from prime_backup.db.access import DbAccess
+
+		recover_count_total = 0
+		with DbAccess.open_session() as session:
+			for fileset_id in self.bad_base_fileset_ids:
+				blob_hashes: Set[str] = set()
+				all_blob_rows: Dict[str, dict] = {}  # hash -> blob row
+				recover_count = 0
+
+				for file in session.get_fileset_files(fileset_id):
+					if file.blob_hash is not None:
+						blob_hashes.add(file.blob_hash)
+						all_blob_rows[file.blob_hash] = {
+							'hash': file.blob_hash,
+							'compress': file.blob_compress,
+							'raw_size': file.blob_raw_size,
+							'stored_size': file.blob_stored_size,
+						}
+
+				existing_blobs = session.get_blobs(list(blob_hashes))
+				for blob_hash in blob_hashes:
+					if blob_hash not in existing_blobs:
+						blob_row = all_blob_rows[blob_hash]
+						session.add(session.create_blob(**blob_row))
+						recover_count += 1
+						recover_count_total += 1
+
+				self.logger.info('Recovered {} database blob for fileset {}'.format(recover_count, fileset_id))
+
+		self.logger.info('Recovered {} database blob in total'.format(recover_count_total))
+
+
+class BlobFileFixer:
+	def __init__(self, blobs_backup_dir: Path):
+		self.blobs_backup_dir = blobs_backup_dir
+
+		from prime_backup import logger
+		self.logger: logging.Logger = logger.get()
+
+	def __find_blob_store_roots(self) -> List[Path]:
+		if not os.path.isdir(self.blobs_backup_dir):
+			self.logger.error('blobs_backup_dir {!r} is not a directory'.format(str(self.blobs_backup_dir)))
+			sys.exit(1)
+
+		from prime_backup.db.access import DbAccess
+		hash_method = DbAccess.get_hash_method()
+
+		self.logger.info('Searching at {!r} for blob storage roots, for hash_method {} with hex length {}'.format(str(self.blobs_backup_dir), hash_method.name, hash_method.value.hex_length))
+		blob_store_roots = []
+		for name1 in os.listdir(self.blobs_backup_dir):
+			path1 = self.blobs_backup_dir / name1
+			if path1.is_dir():
+				for name2 in os.listdir(path1):
+					path2 = path1 / name2
+					if path2.is_dir() and re.fullmatch(r'[0-9a-f]{2}', name2):
+						blob_store_roots.append(path1)
+						self.logger.info('Found blob store root at {!r}'.format(str(path1)))
+						break
+
+		self.logger.info('Found {} blob store roots in total'.format(len(blob_store_roots)))
+		return blob_store_roots
+
+	@classmethod
+	def __try_locate_blob_file(cls, blob_store_roots: List[Path], hash_hex: str) -> Optional[Path]:
+		if len(hash_hex) <= 2:
+			raise ValueError(f'hash {hash_hex!r} too short')
+
+		for blob_store_root in blob_store_roots:
+			blob_file_path = blob_store_root / hash_hex[:2] / hash_hex
+			if blob_file_path.is_file():
+				return blob_file_path
+		return None
+
+	def run(self):
+		blob_store_roots = self.__find_blob_store_roots()
+
+		from prime_backup.db.access import DbAccess
+		from prime_backup.utils import blob_utils
+		prev_missing_blob_count = 0
+		post_missing_blob_count = 0
+		fixed_blob_count = 0
+
+		self.logger.info('Iterating all blobs in the database to check if there is any missing blobs')
+		with DbAccess.open_session() as session:
+			for blobs in session.iterate_blob_batch(batch_size=1000):
+				for blob in blobs:
+					prod_blob_path = blob_utils.get_blob_path(blob.hash)
+					if not prod_blob_path.exists():
+						prev_missing_blob_count += 1
+						recover_blob_path = self.__try_locate_blob_file(blob_store_roots, hash_hex=blob.hash)
+						if recover_blob_path is not None and recover_blob_path.is_file():
+							shutil.copyfile(recover_blob_path, prod_blob_path)
+							fixed_blob_count += 1
+						else:
+							post_missing_blob_count += 1
+
+		self.logger.info('Found {} blobs with their file missing, and recovered {} blob files in total'.format(prev_missing_blob_count, fixed_blob_count))
+		if fixed_blob_count > 0:
+			from prime_backup.action.validate_blobs_action import ValidateBlobsAction
+			self.logger.info('Perform a blob validation since new files were added')
+			result = ValidateBlobsAction().run()
+			if result.bad == 0:
+				self.logger.info('NICE, blob fix done')
+			else:
+				from prime_backup.config.config import Config
+				self.logger.info('There are still {} bad blobs, see {} for more information'.format(result.bad, Config.get().storage_path / 'logs' / 'validate.log'))
+				if len(result.missing) > 0:
+					self.logger.info('There are still {} missing blobs'.format(len(result.missing)))
+				if len(result.missing) == result.bad:
+					self.logger.info('Thankfully all bad blobs are blob-with-missing-file')
+
+
+class Issue64Fixer:
+	def __init__(self, args: argparse.Namespace):
+		from prime_backup import logger
+		self.args = args
+		self.logger: logging.Logger = logger.get()
+		self.__init_pb_environment()
+
+	def run(self):
+		bad_base_fileset_ids = self.__get_bad_base_fileset_ids()
+		if self.args.db_backup_dir:
+			DatabaseFilesetFileFixer(bad_base_fileset_ids, Path(self.args.db_backup_dir)).run()
+			DatabaseBlobFixer(bad_base_fileset_ids).run()
+		if self.args.blobs_backup_dir:
+			BlobFileFixer(Path(self.args.blobs_backup_dir)).run()
+
+	def __init_pb_environment(self):
+		from prime_backup.config.config import Config
+		from prime_backup.db.access import DbAccess
+
+		config = Config.get()
+		config.storage_root = str(Path(self.args.pb_dir).as_posix())
+		self.logger.info('Storage root set to {!r}'.format(config.storage_root))
+
+		try:
+			DbAccess.init(create=False, migrate=False)
+		except Exception as e:
+			self.logger.exception('Load database failed: {}'.format(e))
+			sys.exit(1)
+
+		config.backup.hash_method = DbAccess.get_hash_method()  # use the hash method from the db
+
+	def __get_bad_base_fileset_ids(self) -> List[int]:
+		from prime_backup.action.validate_filesets_action import ValidateFilesetsAction
+		self.logger.info('Locating bad fileset ids')
+
+		result = ValidateFilesetsAction().run()
+		bad_fileset_ids = list(sorted(set(result.bad_filesets.keys())))
+
+		self.logger.info('Found {} bad filesets'.format(len(bad_fileset_ids)))
+
+		from prime_backup.db.access import DbAccess
+		bad_base_fileset_ids: List[int] = []
+		with DbAccess.open_session() as session:
+			filesets = session.get_filesets(bad_fileset_ids)
+			for fileset in filesets.values():
+				if fileset.base_id == 0:
+					bad_base_fileset_ids.append(fileset.id)
+		self.logger.info('Found {} bad base filesets'.format(len(bad_base_fileset_ids)))
+		self.logger.info(', '.join(map(str, bad_base_fileset_ids)))
+
+		return bad_base_fileset_ids
+
+
+def main():
+	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('--pb-plugin', required=True, help='Path to the Prime Backup plugin file')
+	parser.add_argument('--pb-dir', required=True, help='Path to the pb_files directory')
+	parser.add_argument('--db-backup-dir', help='Path to the directory that contains database backup files. All .db files inside will be used. You can ignore this argument if you dont need to recover database file objects')
+	parser.add_argument('--blobs-backup-dir', help='Path to the directory that contains blobs directory backups. Directories inside should be something like blobs1, blobs2, blobs3, where each directory is a valid blobs directory from pb_files/blobs. You can ignore this argument if you dont need to recover blobs')
+	args = parser.parse_args()
+
+	if not os.path.isfile(args.pb_plugin):
+		print('Invalid plugin file: {}'.format(args.pb_plugin))
+		sys.exit(1)
+	sys.path.append(args.pb_plugin)
+	try:
+		import prime_backup
+	except ImportError:
+		print('Failed to import prime_backup, please ensure you provided a correct plugin path. pb_plugin: {!r}'.format(args.pb_plugin))
+		sys.exit(1)
+
+	fixer = Issue64Fixer(args)
+	fixer.run()
+
+
+if __name__ == '__main__':
+	main()

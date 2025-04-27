06fda16d1f2dfb4afc768c3521f0179a3fadf95d
Fallen_Breath <nellaforax@outlook.com>
2025-04-27 17:15:39 +0800

    DeleteBackupTask add summary message for multiple deletion

--- prime_backup/mcdr/task/backup/delete_backup_task.py
+++ prime_backup/mcdr/task/backup/delete_backup_task.py
@@ -7,6 +7,7 @@ from prime_backup.action.delete_backup_action import DeleteBackupAction
 from prime_backup.action.get_backup_action import GetBackupAction
 from prime_backup.mcdr.task.basic_task import HeavyTask
 from prime_backup.mcdr.text_components import TextComponents
+from prime_backup.types.blob_info import BlobListSummary
 from prime_backup.utils import collection_utils
 
 
@@ -31,6 +32,7 @@ class DeleteBackupTask(HeavyTask[None]):
 				TextComponents.backup_id_list(self.backup_ids, hover=False, click=False),
 			)
 
+		bls_total = BlobListSummary.zero()
 		for backup_id in self.backup_ids:
 			backup = GetBackupAction(backup_id).run()
 			if backup.tags.is_protected():
@@ -39,9 +41,17 @@ class DeleteBackupTask(HeavyTask[None]):
 
 			self.reply_tr('deleting', TextComponents.backup_brief(backup, backup_id_fancy=False))
 			dr = DeleteBackupAction(backup_id).run()
+			bls_total += dr.bls
 
 			self.reply_tr(
 				'deleted',
 				TextComponents.backup_id(backup_id, hover=False, click=False),
 				TextComponents.blob_list_summary_store_size(dr.bls),
+			)
+
+		if len(self.backup_ids) > 1:
+			self.reply_tr(
+				'multi_deleted',
+				TextComponents.number(len(self.backup_ids)),
+				TextComponents.blob_list_summary_store_size(bls_total),
 			)

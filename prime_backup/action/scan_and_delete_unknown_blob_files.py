0938a856ce3395aa7289bcc8e798991918c8c3a9
Fallen_Breath <nellaforax@outlook.com>
2025-05-01 13:46:41 +0800

    better file check and logging improve for ScanAndDeleteUnknownBlobFilesAction

--- prime_backup/action/scan_and_delete_unknown_blob_files.py
+++ prime_backup/action/scan_and_delete_unknown_blob_files.py
@@ -1,5 +1,7 @@
 import dataclasses
 import os
+from pathlib import Path
+from typing import List
 
 from typing_extensions import override
 
@@ -21,16 +23,29 @@ class ScanAndDeleteUnknownBlobFilesAction(Action[ScanAndDeleteUnknownBlobFilesRe
 		size_sum = 0
 
 		self.logger.info('Scanning blob store to check if there are any unknown blob files')
+		unknown_blob_file_samples: List[str] = []
 		with DbAccess.open_session() as session:
 			for blob_dir in blob_utils.iterate_blob_directories():
-				file_names = os.listdir(blob_dir)
+				file_names = [
+					name
+					for name in os.listdir(blob_dir)
+					if (blob_dir / name).is_file()
+				]
 				blobs = session.get_blobs(file_names)
 				for blob_hash, blob in blobs.items():
 					if blob is None:
-						unknown_blob_file = blob_dir / blob_hash
+						unknown_blob_file: Path = blob_dir / blob_hash
+						if not unknown_blob_file.is_file():
+							continue
+
 						count += 1
-						size_sum += unknown_blob_file.stat().st_size
-						unknown_blob_file.unlink()
-		self.logger.info('Found {} unknown blob files ({} bytes) in the blob store'.format(count, size_sum))
+						size = unknown_blob_file.stat().st_size
+						size_sum += size
+						self.logger.debug('Found unknown blob at {} with size {}, deleting'.format(unknown_blob_file, size))
+						unknown_blob_file.unlink(missing_ok=True)
+						if len(unknown_blob_file_samples) < 5:
+							unknown_blob_file_samples.append(str(unknown_blob_file))
+
+		self.logger.info('Found and deleted {} unknown blob files ({} bytes) in the blob store, samples: {}'.format(count, size_sum, unknown_blob_file_samples))
 
 		return ScanAndDeleteUnknownBlobFilesResult(count, size_sum)

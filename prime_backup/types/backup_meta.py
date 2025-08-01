34b19ed78d605669295c13bcd8eefe42b3cd2114
Fallen_Breath <nellaforax@outlook.com>
2025-08-02 02:14:41 +0800

    utilize pydantic's BaseModel for the BackupMeta
    for better IDE highlight and type validation

--- prime_backup/types/backup_meta.py
+++ prime_backup/types/backup_meta.py
@@ -1,8 +1,7 @@
 import time
-from typing import List, TYPE_CHECKING, Type, Any, Dict
+from typing import List, TYPE_CHECKING, Any, Dict
 
-from mcdreforged.api.all import Serializable
-from typing_extensions import Self, override
+from pydantic import BaseModel, Field
 
 from prime_backup.types.backup_info import BackupInfo
 from prime_backup.types.operator import Operator
@@ -11,40 +10,31 @@ if TYPE_CHECKING:
 	from prime_backup.db.session import DbSession
 
 
-class BackupMeta(Serializable):
+class BackupMeta(BaseModel):
 	creator: str = str(Operator.unknown())
 	comment: str = ''
-	timestamp_ns: int
-	targets: List[str] = []
-	tags: Dict[str, Any] = {}
+	timestamp_ns: int = Field(default_factory=time.time_ns)
+	targets: List[str] = Field(default_factory=list)
+	tags: Dict[str, Any] = Field(default_factory=dict)
 
 	def to_dict(self) -> dict:
 		return {
 			'_version': 1,
-			**self.serialize(),
+			**self.model_dump(),
 		}
 
 	@classmethod
 	def from_dict(cls, dt: dict) -> 'BackupMeta':
 		version = dt.get('_version', 0)
-		if 'timestamp_ns' not in dt:
-			dt['timestamp_ns'] = time.time_ns()
 
 		# fix https://github.com/TISUnion/PrimeBackup/issues/87
 		# where timestamp_ns was stored as float, and creator was stored as serialized Operator
-		if isinstance(dt['timestamp_ns'], float):
+		if isinstance(dt.get('timestamp_ns', None), float):
 			dt['timestamp_ns'] = int(dt['timestamp_ns'])
 		if isinstance(dt.get('creator', None), dict):
 			dt['creator'] = str(Operator(type=dt['creator']['type'], name=dt['creator']['name']))
 
-		return cls.deserialize(dt)
-
-	@classmethod
-	@override
-	def get_default(cls: Type[Self]) -> Self:
-		obj = super().get_default()
-		obj.timestamp_ns = time.time_ns()
-		return obj
+		return cls.model_validate(dt)
 
 	@classmethod
 	def from_backup(cls, backup: BackupInfo) -> 'BackupMeta':

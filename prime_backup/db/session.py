TISUnion/PrimeBackup

commit e4d9d3b0eccbc333180342ae3f2769b71a22fecf

less `check_sqlite_json_query_support` warning in session.list_backup if filtering is not needed

prime_backup/db/session.py
@@ -1565,13 +1565,9 @@ class DbSession:
 		return helper.get_backups(limit=limit), helper.get_count()
 
 	def list_backup(self, backup_filter: Optional[BackupFilter] = None, limit: Optional[int] = None, offset: Optional[int] = None) -> List[schema.Backup]:
-		if backup_filter is None:
-			backup_filter = BackupFilter()
-
 		s = select(schema.Backup)
-		s = self.__apply_backup_filter(s, backup_filter)
-
-		if self.__needs_manual_backup_tag_filter(backup_filter):
+		if backup_filter is not None and self.__needs_manual_backup_tag_filter(backup_filter):
+			s = self.__apply_backup_filter(s, backup_filter)
 			backups = [backup for backup in self.session.execute(s).scalars().all() if self.__manual_backup_tag_filter(backup, backup_filter)]
 			if offset is not None:
 				backups = backups[offset:]

commit fa552b57baed759a1a3ff806df73fb63bd6d4fd9

fix missing backup filter application in `list_backup`
regression from e4d9d3b0eccbc333180342ae3f2769b71a22fecf

prime_backup/db/session.py
@@ -1565,8 +1565,10 @@ class DbSession:
 
 	def list_backup(self, backup_filter: Optional[BackupFilter] = None, limit: Optional[int] = None, offset: Optional[int] = None) -> List[schema.Backup]:
 		s = select(schema.Backup)
-		if backup_filter is not None and self.__needs_manual_backup_tag_filter(backup_filter):
+		if backup_filter is not None:
 			s = self.__apply_backup_filter(s, backup_filter)
+
+		if backup_filter is not None and self.__needs_manual_backup_tag_filter(backup_filter):
 			backups = [backup for backup in self.session.execute(s).scalars().all() if self.__manual_backup_tag_filter(backup, backup_filter)]
 			if offset is not None:
 				backups = backups[offset:]